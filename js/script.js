d0154a430953b143b269e47e0fb3b8409b97ac87
andrearomeo04 <pisco004@gmail.com>
2024-07-29 01:58:33 +0200

    [UPD] added the api inside the for cicle

--- js/script.js
+++ js/script.js
@@ -3,14 +3,16 @@ const { createApp } = Vue
 createApp({
     data(){
         return {
-
+            emails: []
         }
     },
     methods: {
         randomsEmail () {
             for ( let i = 0; i < 10; i++) {
-                
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                    this.emails.push(res.data.response)
+                })
             }
         }
     }
-})
+}).mount('#app')
