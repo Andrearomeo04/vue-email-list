2d7758d80727035854e386ac924fc1d6f49802d5
andrearomeo04 <pisco004@gmail.com>
2024-07-29 01:53:45 +0200

    [UPD] created random email function and added a for inside it

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue
+
+createApp({
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+        randomsEmail () {
+            for ( let i = 0; i < 10; i++) {
+                
+            }
+        }
+    }
+})
